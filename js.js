8b9a127466af52af4358d2595185c795fc39e40a
earblown <giiiaaat@gmail.com>
2019-05-30 10:20:48 +0300

    basic js idea

--- js.js
+++ js.js
@@ -1,1 +1,16 @@
 //alert("Pick a choise my dude");
+
+var compare = function(myturn, opponent){
+  if (rck===rck){
+    return "rezūltāts neižšķrits";
+
+    else if (myturn=== "rck") {
+      if (opponent==="scissors")
+      return "you win!";
+
+      else {
+
+      }
+    }
+  }
+}
